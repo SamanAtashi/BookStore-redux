21434628a32d492a4121d19a4fc3532818fe9eaa
SamanAtashi <milady00126@gmail.com>
2021-11-01 11:24:34 +0300

    Add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Switch, Route, Link,
+  BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 import Books from './components/Books/Books';
 import Categories from './components/Categories/Categories';
 import AddBook from './components/Books/Book/AddBook';
+import Navigation from './components/Navigation/Navigation';
 
 const App = () => (
   <div>
     <h1>Welcome to BookStore</h1>
     <Router>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
+      <Navigation />
       <Switch>
         <Route path="/categories">
           <Categories />
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Navigation = () => (
+  <nav>
+    <Link to="/">Books</Link>
+    <Link to="/categories">Categories</Link>
+  </nav>
+);
+
+export default Navigation;
