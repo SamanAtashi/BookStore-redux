bc7d23b7ab3f2b91e78b931baf00756a9ebdd9f1
SamanAtashi <milady00126@gmail.com>
2021-11-03 07:21:38 +0300

    Add api to book component

--- src/components/Books/Book/AddBook.js
+++ src/components/Books/Book/AddBook.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../../../redux/books/books';
+import { StoreApiSend } from '../../../API/API_connect';
 
 const AddBook = () => {
   const dispatch = useDispatch();
 
-  const AddBookHandler = () => {
+  const AddBookHandler = async () => {
     const newBook = {
       id: uuidv4(),
       title: document.getElementById('title').value,
       author: document.getElementById('author').value,
     };
-    // dispatch an action and pass it the newBook object (your action's payload)
-    if (newBook.title !== '' && newBook.value !== '') {
+
+    if (
+      document.getElementById('title').value !== '' && document.getElementById('author').value !== ''
+    ) {
+      await StoreApiSend(newBook.title, newBook.author, newBook.id);
       dispatch(addBook(newBook));
     } else {
-      alert('Please fill both inputs');
+      alert('Please fill both inputs!!!');
     }
 
     document.getElementById('title').value = '';
