ae69e4c59fff490a46556dde13464835241da9f3
SamanAtashi <milady00126@gmail.com>
2021-11-05 10:56:52 +0300

    Add style to navbar

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -4,11 +4,11 @@ import Book from './Book/Book';
 import AddBook from './Book/AddBook';
 
 const Books = () => (
-  <>
+  <div className="w-full my-20">
     <ul>
       <Book />
     </ul>
     <AddBook />
-  </>
+  </div>
 );
 export default Books;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FaUserAlt } from 'react-icons/fa';
 
 import { Link } from 'react-router-dom';
 
 const Navigation = () => (
-  <nav>
-    <Link to="/">Books</Link>
-    <Link to="/categories">Categories</Link>
+  <nav className="w-full flex justify-between items-center px-14 py-6 shadow-md">
+    <div className="flex items-center space-x-4">
+      <h2 className="text-blue-600  font-bold text-3xl">
+        Welcome to BookStore
+      </h2>
+      <Link className="font-light text-gray-800" to="/">Books</Link>
+      <Link className="font-light text-gray-800" to="/categories">Categories</Link>
+    </div>
+    <div className="border rounded-full p-4">
+      <FaUserAlt className="text-blue-600" />
+    </div>
   </nav>
 );
 
